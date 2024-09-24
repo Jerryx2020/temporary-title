61e141bf6a9353ed57a708bb7bc39c8d2598bbb4
Jerry Xia <Jerry.xia@mail.utoronto.ca>
2024-09-24 11:15:17 -0400

    deleted unnecessary files

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,30 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Open Data Toronto
+# Author: Jerry Xia
+# Date: 24 September 2024
+# Contact: Jerry.xia@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: None
+# Any other information needed? None
 
 #### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+# Load required libraries
+library(opendatatoronto)  # For accessing Open Data Toronto
+library(tidyverse)        # For data manipulation and saving
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Fetch the DineSafe package using its package ID
+package <- show_package("b6b4f3fb-2e2c-47e7-931d-b87d22806948")
 
+# Get all available resources in this package
+resources <- list_package_resources("b6b4f3fb-2e2c-47e7-931d-b87d22806948")
 
+# Identify datastore resources (CSV/GeoJSON) and filter to CSV for non-geospatial data
+datastore_resources <- filter(resources, tolower(format) == 'csv')
+
+# Load the first datastore resource (DineSafe data)
+dinesafe_data <- get_resource(datastore_resources$id[1])
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+# Save the downloaded data to a CSV file in the raw data folder
+write_csv(dinesafe_data, "data/raw_data/dinesafe_data.csv")
