40f0883484fea04e43546a27b0cf24076ce38b58
Jerry Xia <Jerry.xia@mail.utoronto.ca>
2024-09-25 02:25:20 -0400

    updated simulate, cleaned up files

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -19,9 +19,6 @@ number_of_records <- 1000
 # Establishment types (limited to relevant categories: Restaurant and Food Take Out)
 establishment_types <- c("Restaurant", "Food Take Out")
 
-# Simulating establishment status (e.g., Pass, Conditional Pass, Closed)
-establishment_status <- c("Pass", "Conditional Pass", "Closed")
-
 # Simulating severity levels for infractions (e.g., C - Crucial, M - Minor, S - Significant)
 severity_levels <- c("C - Crucial", "M - Minor", "S - Significant")
 
@@ -32,11 +29,9 @@ number_of_inspections <- sample(1:5, number_of_records, replace = TRUE)
 simulated_data <- 
   tibble(
     establishment_id = rep(paste("ID", 1:(number_of_records/5), sep = "_"), each = 5),  # Each establishment inspected up to 5 times
-    establishment_name = rep(paste("Establishment", 1:(number_of_records/5), sep = "_"), each = 5),
     establishment_type = sample(establishment_types, number_of_records, replace = TRUE),
     inspection_frequency = number_of_inspections,
     severity = sample(severity_levels, number_of_records, replace = TRUE),
-    establishment_status = sample(establishment_status, number_of_records, replace = TRUE)
   )
 
 #### Write to CSV ####
